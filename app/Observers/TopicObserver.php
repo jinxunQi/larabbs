6aa738ca269ae095612b4af42150bc5037524052
574765035 <574765035@qq.com>
2019-08-31 12:56:24 +0000

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -9,18 +9,10 @@ use App\Models\Topic;
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
+    public function saving(Topic $topic)
     {
-        //
-    }
+        $topic->body = clean($topic->body,'user_topic_body');
 
-    public function updating(Topic $topic)
-    {
-        //
-    }
-
-    public function save(Topic $topic)
-    {
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
