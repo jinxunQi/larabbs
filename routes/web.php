15b256c4c345d81ac61996607cb0814371c10a38
574765035 <574765035@qq.com>
2019-08-31 15:13:33 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,8 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');*/
 
 
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
