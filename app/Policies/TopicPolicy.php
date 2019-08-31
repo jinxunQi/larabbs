302f5695bcef4cd679355c039122e15b91e1ddb2
574765035 <574765035@qq.com>
2019-08-31 13:22:01 +0000

    用户可以删除自己发的话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,7 +7,7 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-    public function update(User $user, Topic $topic)
+    /*public function update(User $user, Topic $topic)
     {
         return $topic->user_id == $user->id;
         //return true;
@@ -15,6 +15,17 @@ class TopicPolicy extends Policy
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $topic->user_id == $user->id;
+        //return true;
+    }*/
+    ##重复了代码,进行重构
+    public function update(User $user, Topic $topic)
+    {
+        return $user->isAuthorOf($topic);
+    }
+
+    public function destroy(User $user, Topic $topic)
+    {
+        return $user->isAuthorOf($topic);
     }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -42,15 +42,24 @@
           {!! $topic->body !!}
         </div>
 
+
+        @can('update',$topic)
         <div class="operate">
           <hr>
           <a href="{{ route('topics.edit',$topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
             <i class="far fa-edit"></i> 编辑
           </a>
-          <a href="{{ route('topics.destroy',$topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-            <i class="far fa-trash-alt"></i> 删除
-          </a>
+
+          <form method="post" action="{{ route('topics.destroy',$topic->id) }}" style="display: inline-block;"
+                onsubmit="return confirm('您确定要删除吗？');">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button type="submit" class="btn btn-outline-primary btn-sm">
+              <i class="far fa-trash-alt"></i> 删除
+            </button>
+          </form>
         </div>
+        @endcan
 
       </div>
     </div>
