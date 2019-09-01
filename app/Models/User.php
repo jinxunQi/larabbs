57500abc4c187044431be41ef17eb1ca12829b75
574765035 <574765035@qq.com>
2019-09-01 07:07:05 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
