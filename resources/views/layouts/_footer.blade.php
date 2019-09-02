281051a3023816403e25f45aa03c6e8382e82359
574765035 <574765035@qq.com>
2019-09-02 14:12:45 +0000

    集成 laravel-permission 扩展包

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Jin xun</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://weibo.com/2673630103/profile?topnav=1&wvr=6&is_all=1" target="_blank">Jin xun</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="mailto:574765035@qq.com">联系我们</a></p>
