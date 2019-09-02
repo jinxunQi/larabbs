ef0b4c96ba8fc66a6a2e3f1f342d07ad5a5db5c2
574765035 <574765035@qq.com>
2019-09-02 14:30:55 +0000

    站点权限部署

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 
     /**
