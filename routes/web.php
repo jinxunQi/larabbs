fc39377b852c7b30cad9f1c02addc4ab7c57cd91
574765035 <574765035@qq.com>
2019-09-03 06:44:01 +0000

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 Route::resource('notifications', 'NotificationController', ['only' => ['index']]);
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
