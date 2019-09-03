3d528a6cbc1cfd332cac23745ab076f8428210fb
574765035 <574765035@qq.com>
2019-09-03 07:39:32 +0000

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -17,11 +18,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
 	    $topics = $topic->WithOrder($request->order)->paginate(20);
+	    $active_users = $user->getActiveUsers();
+
 		/*$topics = Topic::with('user','category')->paginate(20);*/
-		return view('topics.index', compact('topics'));
+		return view('topics.index', compact('topics','active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
